1e988a392846bec4aab7c174c69c5f178deb7435
Arnab Kanti Tarafder <47710249+Arnab9Codes@users.noreply.github.com>
2023-02-04 21:09:22 -0500

    Create spmv_coo_format.c
    does matrix vector multiplication in coo format

---
+++ spmv_coo/spmv_coo_format.c
@@ -0,0 +1,56 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "COO.h"
+
+
+//#define NUM_THREADS 4 // same as number of rows
+#define R1 4
+#define C1 4
+//#define R2 4
+//#define C2 4
+
+
+ double* spmv(COO coo, double V[C1]){
+
+    static double res[R1];
+
+    #pragma omp parallel for
+    for(int i=0;i<coo.nzeros;i++){
+        int r=coo.rptrs[i];
+        int c=coo.clmnptrs[i];
+
+        double m_val=coo.vals[i];
+        int v=V[c];
+
+        #pragma omp atomic
+        res[r]+=m_val*v;
+    }
+    return res;
+}
+
+int main()
+{
+    double m[R1][C1]={{1,0,0,0},
+            {0,1,0,0},
+            {0,0,0,1},
+            {1,0,0,1}};
+
+    double v[C1]={1,1,1,1};
+    double* product;
+
+    for(int i=0;i<R1;i++){
+            product[i]=0.0;
+    }
+
+    COO coo_object1, coo_object2;
+    coo_object1=coo_generation(4,4,5,m);
+    product=spmv(coo_object1,v);
+    //print_coo(coo_object1);
+    for(int i=0;i<R1;i++){
+        printf("%lf ", *(product+i));
+    }
+
+    return 0;
+}
+    
